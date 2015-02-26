d739c643406baceb0a6ce38250ae867a1b59d418
CQD <thecqd@gmail.com>
2015-02-26 18:54:22 +0800

    init

---
+++ public/index.php
@@ -0,0 +1,68 @@
+<?php
+$actual_link = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+
+$input = @$_SERVER['REQUEST_URI'];
+$input = @trim($input);
+
+if (empty($input) || '/'===$input) {
+    $input = "抖";
+} else {
+    $input = substr($input, 1);
+    $input = urldecode($input);
+}
+
+$len = strlen($input);
+$base = 20;
+$font_size = ($len>5) ? ($base*5/$len) : $base;
+?><!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#">
+<head>
+<meta charset="utf-8">
+<title>抖<?=htmlspecialchars($input)?></title>
+<meta property="og:title" content="抖<?=htmlspecialchars($input)?>" />
+<meta property="og:url" content="<?=htmlspecialchars($actual_link)?>" />
+<meta property="og:description" content="抖你的這個<?=htmlspecialchars($input)?>" />
+<meta property="og:image" content="http://<?=$_SERVER[HTTP_HOST]?>/img/og.png" />
+
+<link rel="stylesheet" type="text/css" href="http://elrumordelaluz.github.io/csshake/css/csshake.css">
+<style>
+html,body{margin:0;padding:0;background-color:#FFAB52;color:white;height:100%; overflow:hidden}
+.wrapper{
+    text-align:center;
+    padding:2em 1em 3em;
+    width:100%;
+    height:100%;
+    max-width:1500px;
+    margin:0 auto;
+    box-sizing:border-box;
+}
+.S{
+    font-size:<?=(double) $font_size?>em;
+    line-height:120%;
+}
+
+table{
+width:100%;
+height:90%;
+}
+</style>
+</head>
+<body>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-368263-15', 'xn--pru.hiigara.net');
+ga('send', 'pageview');
+</script>
+<div class="wrapper">
+<table>
+<tr><td valign="middle">
+<span class="S shake shake-hard shake-constant"><?=htmlspecialchars($input)?></span>
+</td></tr></table>
+</div>
+</body>
+</html>
+
